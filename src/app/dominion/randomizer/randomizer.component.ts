e698e2574b4fa96678b98850c452f9cb663965c4
Hideaki <noshiro.pf@gmail.com>
2017-05-30 03:50:58 +0900

    randomizer mylib::getRandomValue( array )

--- src/app/dominion/randomizer/randomizer.component.ts
+++ src/app/dominion/randomizer/randomizer.component.ts
@@ -23,15 +23,27 @@ export class RandomizerComponent implements OnInit {
 
     DominionSetList: { name: string, selected: boolean }[];
     CardPropertyList: CardProperty[] = [];
+
     SelectedCards : {
-        KingdomCards : number[],
-        Prosperity   : boolean,
-        DarkAges     : boolean,
+        KingdomCards    : number[],
+        Prosperity      : boolean,
+        DarkAges        : boolean,
+        BaneCard        : number,
+        EventCards      : number[],
+        LandmarkCards   : number[],
+        Obelisk         : number,
+        BlackMarketPile : number[],
     } = {
-        KingdomCards : [],
-        Prosperity   : false,
-        DarkAges     : false,        
+        KingdomCards    : [],
+        Prosperity      : false,
+        DarkAges        : false,
+        BaneCard        : 0,
+        EventCards      : [],
+        LandmarkCards   : [],
+        Obelisk         : 0,
+        BlackMarketPile : [],
     };
+
     constructor(
         private mylib: MyLibraryService,
         private httpGetSetListService: GetSetListService,
@@ -56,26 +68,81 @@ export class RandomizerComponent implements OnInit {
 
 
     randomizer() {
-        this.SelectedCards.KingdomCards
-         = this.mylib.shuffle( this.CardPropertyList
-            .filter( e => e.category == '王国' )
+        // reset
+        this.SelectedCards.KingdomCards    = [];
+        this.SelectedCards.Prosperity      = false;
+        this.SelectedCards.DarkAges        = false;
+        this.SelectedCards.BaneCard        = 0;
+        this.SelectedCards.EventCards      = [];
+        this.SelectedCards.LandmarkCards   = [];
+        this.SelectedCards.Obelisk         = 0;
+        this.SelectedCards.BlackMarketPile = [];
+
+
+        // 選択されている拡張セットに含まれているカードすべてをシャッフルし，indexとペアにしたリスト
+        const RandomSelectedShuffled
+         = this.mylib.shuffle(
+            this.CardPropertyList
+            .map( (val,index) => { return { index: index, data: val }; } )
             .filter( e =>
                 this.DominionSetList
                 .filter( s => s.selected )
                 .map( s => s.name )
-                .findIndex( val => val == e.set_name ) >= 0 )
-            .map( e => e.no )
-        ).slice( 0, 10 );
+                .findIndex( val => val == e.data.set_name ) >= 0 )
+         );
 
+        // KingdomCards
+        this.SelectedCards.KingdomCards
+         = RandomSelectedShuffled
+            .filter( e => e.data.category == '王国' )
+            .slice( 0, 10 )
+            .map( e => e.index );
 
+        // 繁栄場・避難所場の決定
         this.SelectedCards.Prosperity = ( this.CardPropertyList[ this.SelectedCards.KingdomCards[0] ].set_name === '繁栄' );
         this.SelectedCards.DarkAges   = ( this.CardPropertyList[ this.SelectedCards.KingdomCards[9] ].set_name === '暗黒時代' );
 
+        this.SelectedCards.KingdomCards.sort();  // 繁栄場・避難所場の決定後にソート
 
-        // .sort();
+
+        // 災いカード（収穫祭：魔女娘）
+        this.SelectedCards.BaneCard
+         = this.mylib.getRandomValue(
+            RandomSelectedShuffled
+                .filter( e => e.data.category == '王国' )
+                .slice(11)
+                .filter( e => (
+                       e.data.cost.debt   == 0
+                    && e.data.cost.potion == 0
+                    && e.data.cost.coin   >= 2
+                    && e.data.cost.coin   <= 3 ) )
+                .map( e => e.index )
+            );
 
 
-        console.log( this.SelectedCards );
+        // Event Cards & Landmark Cards
+        RandomSelectedShuffled
+            .filter( e => e.data.card_type == 'イベント' || e.data.card_type == 'ランドマーク' )
+            .slice( 0, this.mylib.RandomNumber(0,2) )
+            .forEach( e => {
+                if ( e.data.card_type == 'イベント' ) {
+                    this.SelectedCards.EventCards.push( e.index );
+                } else if ( e.data.card_type == 'ランドマーク' ) {
+                    this.SelectedCards.LandmarkCards.push( e.index );
+                }
+            });
+
+        // Black Market
+
+        // Obelisk (Choose 1 Action Supply Pile)
+
+        console.log(
+            this.SelectedCards.KingdomCards.map( e => this.CardPropertyList[e].name_jp ),
+            this.CardPropertyList[ this.SelectedCards.BaneCard ].name_jp,
+            this.SelectedCards.EventCards.map( e => this.CardPropertyList[e].name_jp ),
+            this.SelectedCards.LandmarkCards.map( e => this.CardPropertyList[e].name_jp ),
+        );
+
     }
 
 }
--- src/app/my-library.service.ts
+++ src/app/my-library.service.ts
@@ -3,85 +3,111 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MyLibraryService {
 
-  constructor() { }
+    constructor() { }
 
-  RandInt( Min: number, Max: number ): number {
-    return Min + Math.floor( Math.random() * ( Max - Min + 1 ) );
-  }
+    RandomNumber( Min: number, Max: number ): number {
+        return Min + Math.floor( Math.random() * ( Max - Min + 1 ) );
+    }
 
-  isEmpty( ar: any[] ): boolean {
-    return ar.length === 0;
-  }
+    isEmpty( ar: any[] ): boolean {
+        return ar.length === 0;
+    }
 
-  back<T>( ar: Array<T> ): T {
-    return ar[ ar.length - 1 ];
-  };
-
-
-  removeAt<T>( ar: Array<T>, index: number ): Array<T> {
-    return ar.splice( index, 1 );
-  };
-
-  removeValue<T>( ar: Array<T>, target: T ): Array<T> {
-    return ar.filter( e => e != target );
-  };
-
-  append( ar1: any[], ar2: any[] ): any[] {
-    return [].concat( ar1, ar2 );
-  }
+    back<T>( ar: Array<T> ): T {
+        return ar[ ar.length - 1 ];
+    };
 
 
+    removeAt<T>( ar: Array<T>, index: number ): Array<T> {
+        return ar.splice( index, 1 );
+    };
 
-  // let a = [ 1,2,3,[1,2,3],5 ];
-  // let b = this.mylib.makeShallowCopy(a);
-  // a[2] = 999;
-  // a[3][1] = 9999;
-  // console.log( a,b );
-  copy<T>( ar: Array<T> ): Array<T> {
-    return [].concat( ar );
-  }
+    removeValue<T>( ar: Array<T>, target: T ): Array<T> {
+        return ar.filter( e => e != target );
+    };
 
-
-  shallowCopy( obj, asArray?: boolean ) {
-    if ( asArray ) return Object.assign([], obj);
-    return Object.assign({}, obj);
-  }
-
-  filterRemove<T>( array: Array<T>, f: (T) => boolean ): [ Array<T>, Array<T> ] {
-    let others = array.filter( (e) => !f(e) );
-    return [ array.filter(f), others ];
-  };
-
-  // copy and return unique array
-  // 要素の値を定義する関数（この値の同値性でuniqをかける
-  uniq<T>( ar: Array<T>, f: (T) => any = ( (e) => e ) ) {
-    return ar.map( (e) => [ e, f(e) ] )
-        .filter( (val, index, array ) => (array.map( a => a[1] ).indexOf( val[1] ) === index) )
-        .map( a => a[0] );
-  };
-
-  sortNumeric( array: string[] ): string[] {
-    return array.sort( (a,b) => ( parseFloat(a) - parseFloat(b) ) );
-  };
+    append( ar1: any[], ar2: any[] ): any[] {
+        return [].concat( ar1, ar2 );
+    }
 
 
-  shuffle( array: any[] ): any[] {
-    return array
-        .map( (e) => [e, Math.random()] )
-        .sort( (x, y) => x[1] - y[1] )
-        .map( (pair) => pair[0] );
-  }
 
-  permutation( n: number ): number[] { 
-    let ar = new Array<number>(n);
-    for ( let i = 0; i < n; ++i ) { ar[i] = i; }
-    return this.shuffle( ar );
-  }
+    // let a = [ 1,2,3,[1,2,3],5 ];
+    // let b = this.mylib.makeShallowCopy(a);
+    // a[2] = 999;
+    // a[3][1] = 9999;
+    // console.log( a,b );
+    copy<T>( ar: Array<T> ): Array<T> {
+        return [].concat( ar );
+    }
 
 
-   sleep( sec: number ): Promise<any> {
-    return new Promise( resolve => setTimeout( resolve, sec * 1000 ) );
-  }
+    shallowCopy( obj, asArray?: boolean ) {
+        if ( asArray ) return Object.assign([], obj);
+        return Object.assign({}, obj);
+    }
+
+    filterRemove<T>( array: Array<T>, f: (T) => boolean ): [ Array<T>, Array<T> ] {
+        let others = array.filter( (e) => !f(e) );
+        return [ array.filter(f), others ];
+    };
+
+    // copy and return unique array
+    // 要素の値を定義する関数（この値の同値性でuniqをかける
+    uniq<T>( ar: Array<T>, f: (T) => any = ( (e) => e ) ) {
+        return ar.map( (e) => [ e, f(e) ] )
+                .filter( (val, index, array ) => (array.map( a => a[1] ).indexOf( val[1] ) === index) )
+                .map( a => a[0] );
+    };
+
+    sortNumeric( array: string[] ): string[] {
+        return array.sort( (a,b) => ( parseFloat(a) - parseFloat(b) ) );
+    };
+
+
+    shuffle( array: any[] ): any[] {
+        return array
+                .map( (e) => [e, Math.random()] )
+                .sort( (x, y) => x[1] - y[1] )
+                .map( (pair) => pair[0] );
+    }
+
+    permutation( n: number ): number[] { 
+        let ar = new Array<number>(n);
+        for ( let i = 0; i < n; ++i ) { ar[i] = i; }
+        return this.shuffle( ar );
+    }
+
+    setDifference( sortedArray1: number[], sortedArray2: number[] ): number[] {
+        let result: number[] = [];
+        let it1 = 0;  // iterator for sortedArray1
+        let it2 = 0;  // iterator for sortedArray2
+        let val1 = sortedArray1[it1];
+        let val2 = sortedArray2[it2];
+        while ( it1 < sortedArray1.length && it2 < sortedArray2.length ) {
+            if ( val1 == val2 ) {
+                val1 = sortedArray1[++it1];
+                val2 = sortedArray2[++it2];
+            } else if ( val1 < val2 ) {
+                result.push(val1);
+                val1 = sortedArray1[++it1];
+            } else {
+                val2 = sortedArray2[++it2];
+            }
+        }
+        for ( ; it1 < sortedArray1.length; ++it1 ) {
+            result.push( sortedArray1[it1] );
+        }
+        return result;
+    }
+
+    getRandomValue<T>( array: Array<T> ): T{
+        return array[ this.RandomNumber( 0, array.length - 1 ) ];
+    }
+
+    sleep( sec: number ): Promise<any> {
+        return new Promise( resolve => setTimeout( resolve, sec * 1000 ) );
+    }
 
 
 }
@@ -91,12 +117,12 @@ export class MyLibraryService {
 // // let ar = [3,6,7,9,1,2,4,5];
 // // console.log( NotUsedNumber([], 2) ); -> 8
 // NotUsedNumber( NumberArray: number[], start_number: number ): number {
-//   let sorted: number[] = NumberArray.sort();
-//   while ( sorted[0] < start_number ) sorted.shift();
-//   if ( sorted.length === 0 ) return start_number;
-//   for ( let i = 0; i < sorted.length; ++i ) {
-//     if ( Number( sorted[i] ) !== start_number + i ) return start_number + i;
-//   }
-//   return start_number + sorted.length;
+//     let sorted: number[] = NumberArray.sort();
+//     while ( sorted[0] < start_number ) sorted.shift();
+//     if ( sorted.length === 0 ) return start_number;
+//     for ( let i = 0; i < sorted.length; ++i ) {
+//         if ( Number( sorted[i] ) !== start_number + i ) return start_number + i;
+//     }
+//     return start_number + sorted.length;
 // }
 
